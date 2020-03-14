d37c849f63865785340d79123472cb868fa28c0f
KayacChang <egg734631@gmail.com>
2020-03-14 13:19:20 +0800

    Add test/bet

--- test/fake_server/main.js
+++ test/fake_server/main.js
@@ -2,7 +2,7 @@ const restify = require("restify");
 
 const prefix = "/api/v1/tgc";
 
-function respond(req, res, next) {
+function account(req, res, next) {
   const respond = {
     data: true,
     balance: {
@@ -20,8 +20,38 @@ function respond(req, res, next) {
   next();
 }
 
+function bet(req, res, next) {
+  const {
+    account,
+    eventTime,
+    gamehall,
+    gamecode,
+    roundid,
+    amount,
+    mtcode
+  } = req.body;
+
+  const respond = {
+    data: {
+      balance: 600270,
+      currency: "CNY"
+    },
+    status: {
+      code: "0",
+      message: "Success",
+      datetime: new Date().toISOString()
+    }
+  };
+
+  res.send(respond);
+  next();
+}
+
 var server = restify.createServer();
-server.get(`${prefix}/player/check/:account`, respond);
+
+server.get(`${prefix}/player/check/:account`, account);
+
+server.post(`${prefix}/transaction/game/bet`);
 
 server.listen(3000, function() {
   console.log("%s listening at %s", server.name, server.url);
