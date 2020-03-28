7927b684f84056d4f0ad755219e04375926f563a
張同學 <egg734631@sixonetech.com>
2020-03-28 23:00:14 +0800

    Feature/add put game

--- src/usecase/game/usecase.go
+++ src/usecase/game/usecase.go
@@ -51,3 +51,27 @@ func (it *Usecase) Store(name, href, category string) (*model.Game, error) {
 
 	return &game, nil
 }
+
+func (it *Usecase) Update(name string, req map[string]string) (*model.Game, error) {
+
+	game, err := it.Find(name)
+	if err != nil {
+		return nil, err
+	}
+
+	if req["name"] != "" {
+		game.Name = req["name"]
+	}
+	if req["href"] != "" {
+		game.Href = req["href"]
+	}
+	if req["category"] != "" {
+		game.Category = req["category"]
+	}
+
+	if err := it.repo.Update(game); err != nil {
+		return nil, err
+	}
+
+	return game, nil
+}
