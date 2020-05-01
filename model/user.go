3326df23829f898764abbb4964c33409e48d9644
張同學 <egg734631@sixonetech.com>
2020-05-01 15:06:53 +0800

    Fix/fix user db metatag

--- model/user.go
+++ model/user.go
@@ -2,12 +2,13 @@ package model
 
 import "time"
 
+// User ...
 type User struct {
-	ID       string  `json:"user_id"`
-	Username string  `json:"username"`
-	Balance  float64 `json:"balance"`
-	Session  string  `json:"session"`
+	ID       string  `json:"user_id" db:"user_id"`
+	Username string  `json:"username" db:"username"`
+	Balance  float64 `json:"balance" db:"balance"`
+	Session  string  `json:"session" db:"session"`
 
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
