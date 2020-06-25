ccee765aeb9358a714969c6089f25d36d19f2773
張同學 <egg734631@gmail.com>
2020-06-26 00:53:29 +0800

    Update framework/server/chi.go

--- framework/server/chi.go
+++ framework/server/chi.go
@@ -23,7 +23,7 @@ func New(e env.Env) Server {
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Session"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		MaxAge:           300,
